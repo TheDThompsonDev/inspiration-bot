e7e25119aea2b31d774b5d85a7ea406548e52e7e
Tom Schmelzer <30869493+schmelto@users.noreply.github.com>
2022-02-28 16:14:08 +0100

    docs: add quote (#8)

--- src/config/QuoteList.ts
+++ src/config/QuoteList.ts
@@ -5,4 +5,8 @@ export const QuoteList: Quote[] = [
     text: "One day I'll put actual quotes in here!",
     author: "Naomi Carrigan",
   },
+  {
+    text: "Whether you think you can, or you think you can’t – you’re right.",
+    author: "Henry Ford",
+  },
 ];
