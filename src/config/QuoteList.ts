bfa2cf551341c8d391ef445986ba6204651f9e4d
Danny Thompson <dannythompson901@gmail.com>
2022-03-01 10:43:44 -0600

    Added 2 quotes to QuoteList.ts

--- src/config/QuoteList.ts
+++ src/config/QuoteList.ts
@@ -9,4 +9,12 @@ export const QuoteList: Quote[] = [
     text: "Whether you think you can, or you think you can’t – you’re right.",
     author: "Henry Ford",
   },
+  {
+    text: "Supporting another developers success will NEVER ruin yours.",
+    author: "Danny Thompson",
+  },
+  {
+    text: "Believe you can and you’re halfway there.",
+    author: "Theodore Roosevelt",
+  },
 ];
